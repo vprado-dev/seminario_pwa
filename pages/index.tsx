599f24d0706e29236b8a46863733411e0b2ac47b
Vinicius Prado <vinicavagna@gmail.com>
2023-01-31 14:42:09 -0300

    :wrench: now using Next own Image tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,44 @@
 import { useState } from "react";
 import styles from "../styles/Home.module.css";
+import Image from "next/image";
 
 export default function Home() {
   const coffees = [
     {
       name: "Perspiciatis",
-      image: "images/coffee1.jpg",
+      image: "/images/coffee1.jpg",
     },
     {
       name: "Voluptatem",
-      image: "images/coffee2.jpg",
+      image: "/images/coffee2.jpg",
     },
     {
       name: "Explicabo",
-      image: "images/coffee3.jpg",
+      image: "/images/coffee3.jpg",
     },
     {
       name: "Rchitecto",
-      image: "images/coffee4.jpg",
+      image: "/images/coffee4.jpg",
     },
     {
       name: " Beatae",
-      image: "images/coffee5.jpg",
+      image: "/images/coffee5.jpg",
     },
     {
       name: " Vitae",
-      image: "images/coffee6.jpg",
+      image: "/images/coffee6.jpg",
     },
     {
       name: "Inventore",
-      image: "images/coffee7.jpg",
+      image: "/images/coffee7.jpg",
     },
     {
       name: "Veritatis",
-      image: "images/coffee8.jpg",
+      image: "/images/coffee8.jpg",
     },
     {
       name: "Accusantium",
-      image: "images/coffee9.jpg",
+      image: "/images/coffee9.jpg",
     },
   ];
 
@@ -55,7 +56,13 @@ export default function Home() {
         <div className={styles.container}>
           {coffees.map(({ name, image }) => (
             <div className={styles.card}>
-              <img className={styles["card--avatar"]} src={image} />
+              <Image
+                className={styles["card--avatar"]}
+                src={image}
+                width={240}
+                height={160}
+                alt={name}
+              />
               <h1 className={styles["card--title"]}>{name}</h1>
               <a className={styles["card--link"]} href="#">
                 Taste
