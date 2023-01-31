dc991726f5f66718130f6346e76908ea55504c9a
Vinicius Prado <vinicavagna@gmail.com>
2023-01-31 00:59:03 -0300

    :lipstick: add gif state and style button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,42 @@
-import styles from '../styles/Home.module.css'
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [showGif, setShowGif] = useState(false);
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Fim do Seminário PWA!
-        </h1>
+        <h1 className={styles.title}>Fim do Seminário PWA!</h1>
 
-        <p className={styles.description}>
-          Obrigado pela atenção!
-        </p>
+        <p className={styles.description}>Obrigado pela atenção!</p>
 
         <div className={styles.grid}>
-          <a className={styles.card}>
-            <h3>Gif</h3>
+          <a className={styles.button} onClick={() => setShowGif(!showGif)}>
+            <p style={{fontSize: 20}}>Gif</p>
           </a>
         </div>
+        {showGif && (
+          <>
+            <div
+              style={{
+                width: "100%",
+                height: "0",
+                paddingBottom: "56%",
+                position: "relative",
+              }}
+            >
+              <img
+                src="https://media3.giphy.com/media/gG6OcTSRWaSis/giphy.gif?cid=790b7611b9ad38c5ecfe2903b5d2f5007e0cc7963db92632&rid=giphy.gif&ct=g"
+                width="100%"
+                height="100%"
+                style={{ position: "absolute" }}
+                className="giphy-embed"
+              />
+            </div>
+          </>
+        )}
       </main>
     </div>
-  )
+  );
 }
